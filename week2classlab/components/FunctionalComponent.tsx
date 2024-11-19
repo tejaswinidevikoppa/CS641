378fe1d79adc0bdb1de7aeb015a36d793b9c534a
TEJASWINI D S <tejashwinidevikoppa@gmail.com>
2024-11-18 23:29:49 -0500

    changes

--- week2classlab/components/FunctionalComponent.tsx
+++ week2classlab/components/FunctionalComponent.tsx
@@ -2,53 +2,65 @@ import React, { useState, useEffect } from "react";
 import { View, Text, Button } from "react-native";
 import customStyle from "../styles/custom";
 
-interface FunctionalComponentProps
-{
+interface FunctionalComponentProps {
     buttonTitle: string;
     showButton: boolean;
 }
-
 
 const FunctionalComponent = (props: FunctionalComponentProps) => {
     const [count, setCount] = useState(100);
     const [onScreenTimer, setOnScreenTimer] = useState(0);
     const [dummyText, setDummyText] = useState('Initial');
+
+    // Increase count
     function increase() {
         setCount(count + 1);
     }
 
+    // Users data
     const users = [
-        {firstName: 'A', lastName: 'B'},
-        {firstName: 'C', lastName: 'D'},
-        {firstName: 'E', lastName: 'F'},
-
+        { firstName: 'A', lastName: 'B' },
+        { firstName: 'C', lastName: 'D' },
+        { firstName: 'E', lastName: 'F' },
     ];
 
+    // Handle button press
     function handleButtonPress() {
         setDummyText('Button pressed');
     }
 
+    // Decrease count
     function decrease() {
         setCount(count - 1);
     }
 
+    // Effect to manage timer
     useEffect(() => {
-        const value = setInterval(() => setOnScreenTimer((onScreenTimer) => onScreenTimer + 2), 2000)
+        const value = setInterval(() => setOnScreenTimer((onScreenTimer) => onScreenTimer + 2), 2000);
         return () => clearInterval(value);
     }, []);
-    
+
     return (
         <View style={customStyle.container}>
-           <Text>Time on screen : {onScreenTimer}</Text>
-           <Text>{dummyText}</Text>
-            {users.map((user) => (
-                <Text>{user.lastName}, {user.firstName}</Text>
-            ))}
-           <Button title={props.buttonTitle} onPress={increase} disabled></Button>
-           { props.showButton ?
-            <Button title={props.buttonTitle} onPress={handleButtonPress}></Button> : <Text>Ternary False</Text>}
+            <Text>Time on screen: {onScreenTimer}</Text>
+            <Text>{dummyText}</Text>
+
+            {/* Render list of users with a unique key prop */}
+            {users.map((user, index) => {
+                return <Text key={index}>{user.lastName}, {user.firstName}</Text>;
+            })}
+
+            {/* Increase count button */}
+            <Button title={props.buttonTitle} onPress={increase} disabled />
+
+            {/* Conditional rendering of the second button */}
+            {props.showButton ? (
+                <Button title={props.buttonTitle} onPress={handleButtonPress} />
+            ) : (
+                <Text>Ternary False</Text>
+            )}
         </View>
     );
-}
+};
 
 export default FunctionalComponent;
